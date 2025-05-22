1ec66c536ab928937c46f4fce44b330eb75f373d
Tanmay Taneja <tanmaytaneja2@gmail.com>
2025-05-23 01:33:11 +0530

    Introduced Synthetic Ticks

--- vwap/main.cpp
+++ vwap/main.cpp
@@ -1,34 +1,27 @@
 #include "vwap.hpp"
 
 #include <iostream>
+#include <x86intrin.h> //? For __rdtsc (works with GCC/Clang on x86/x64)
 
 #define DEFAULT_WINDOW_TIME 30
 #define DEFAULT_SIMULATION_TIME 50
 
 template <typename PriceType>
-TickData<PriceType> generateRandomTick(int &currentTime)
+TickData<PriceType> generateSyntheticTick(int currentTime)
 {
-    PriceType price{0};
-    int volume{0};
-    std::cout << "Price(" << ++currentTime << "): ";
-    std::cin >> price;
-    std::cout << "Volume(" << currentTime << "): ";
-    std::cin >> volume;
-    return TickData<PriceType>{price, volume, currentTime};
+    return TickData<PriceType>(static_cast<PriceType>(100.0 + (currentTime % 10)), 1 + (currentTime % 5), currentTime);
 }
 
 int main()
 {
+    constexpr int NUM_TICKS = 1'000'000;
+
     std::cout << "VWAP Calculator" << std::endl;
-    int currentTime = 0;
     int windowTime = DEFAULT_WINDOW_TIME;
     int simulationTime = DEFAULT_SIMULATION_TIME;
-    std::cout << "Window Time (in seconds): ";
-    std::cin >> windowTime;
-    std::cout << "Simulation Time (in seconds): ";
-    std::cin >> simulationTime;
 
     std::cout << "Simulation Start" << std::endl;
+    std::cout << std::endl;
 
     VWAPCalculator<double> vWAPCalculator(windowTime);
     /* or
@@ -37,13 +30,20 @@ int main()
     ? only use when dynamic lifetime of object is required.
     */
 
-    while (simulationTime--)
+    unsigned long long start = __rdtsc();
+
+    for (int currentTime = 0; currentTime < NUM_TICKS; ++currentTime)
     {
-        std::cout << std::endl;
-        vWAPCalculator.insertTick(generateRandomTick<double>(currentTime));
-        std::cout << "Current VWAP: " << vWAPCalculator.getVWAP() << std::endl
-                  << std::endl;
+        TickData<double> syntheticTick = generateSyntheticTick<double>(currentTime);
+        vWAPCalculator.insertTick(syntheticTick);
     }
+
+    unsigned long long end = __rdtsc();
+
+    std::cout << "Final VWAP: " << vWAPCalculator.getVWAP() << std::endl;
+    std::cout << "Total CPU Cycles: " << end - start << std::endl;
+    std::cout << "Average CPU Cycles per insertTick(): " << (end - start) / NUM_TICKS << std::endl;
+    std::cout << std::endl;
 
     std::cout << "Simulation End" << std::endl;
 
--- vwap/vwap.hpp
+++ vwap/vwap.hpp
@@ -4,13 +4,17 @@
 #include <iostream>
 #include <queue>
 
+static constexpr int MAX_TICKS = 1024;
+
 /* 16 Bytes */
 template <typename PriceType>
-struct alignas(16) TickData // TODO: struct cache aligning
+struct /* alignas(16) */ TickData // TODO: struct cache aligning
 {
     PriceType price;
     int volume;
     int timestamp;
+
+    TickData(PriceType price, int volume, int timestamp) : price{price}, volume{volume}, timestamp{timestamp} {}
     // static_assert(sizeof(TickData<PriceType>) == 16, "struct TickData<PriceType> is not tightly packed!");
 };
 
@@ -22,7 +26,7 @@ public:
     {
         while (!tickWindow.empty() && tickData.timestamp - tickWindow.front().timestamp >= windowSeconds)
         {
-            const auto& old = tickWindow.front();
+            const auto &old = tickWindow.front();
             VWAPSum -= old.price * old.volume;
             totalVolume -= old.volume;
             tickWindow.pop();
@@ -32,7 +36,7 @@ public:
         totalVolume += tickData.volume;
     }
 
-    double getVWAP() const
+    inline double getVWAP() const
     {
         return VWAPSum / totalVolume;
     }
@@ -42,14 +46,20 @@ public:
         // TODO: tickWindow queue resizing ?? it's according to timestamp though, so maybe size can be indefinite?
     }
 
-    VWAPCalculator(const VWAPCalculator&) = delete;
-    VWAPCalculator& operator=(const VWAPCalculator&) = delete;
+    VWAPCalculator(const VWAPCalculator &) = delete;
+    VWAPCalculator &operator=(const VWAPCalculator &) = delete;
+    VWAPCalculator(VWAPCalculator &&) = delete;
 
 private:
     int windowSeconds;
     double VWAPSum;
     int totalVolume;
-    std::queue<TickData<PriceType>> tickWindow;
+    std::queue<TickData<PriceType>> tickWindow; //! Never use std::queue in HFT since it is backed by std::deque, this causes dynamic allocations and non-contiguous memory.
+
+    // std::array<TickData<PriceType>, MAX_TICKS> tickWindow; //? Use this instead. This represents a ring-buffer. It offers us ZERO dynamic memory allocations, constant time push/pop and predictable memory layout.
+    // size_t head = 0;
+    // size_t tail = 0;
+    // size_t size = 0;
 };
 
 #endif
