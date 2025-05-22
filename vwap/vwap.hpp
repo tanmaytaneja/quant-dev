ee9449b75c526d0f2e61cab0c48a009353ace238
Tanmay Taneja <tanmaytaneja2@gmail.com>
2025-05-23 02:55:35 +0530

    Optimized Queue to Array so that there is no dynamic memory allocation and memory is contiguous

--- vwap/vwap.hpp
+++ vwap/vwap.hpp
@@ -1,21 +1,25 @@
 #ifndef VWAP_HPP
 #define VWAP_HPP
 
+#define TICK_WINDOW_QUEUE 0
+
 #include <iostream>
 #include <queue>
 
 static constexpr int MAX_TICKS = 1024;
+static constexpr int TICK_MASK = MAX_TICKS - 1; //? Ensure MAX_TICKS is a power of 2
 
 /* 16 Bytes */
 template <typename PriceType>
-struct /* alignas(32) */ TickData // TODO: struct cache aligning
+struct alignas(32) TickData // TODO: struct cache aligning
 {
     PriceType price;
     int volume;
     int timestamp;
 
-    TickData(PriceType price, int volume, int timestamp) : price{price}, volume{volume}, timestamp{timestamp} {}
-    // static_assert(sizeof(TickData<PriceType>) == 16, "struct TickData<PriceType> is not tightly packed!");
+    TickData() = default; //? Required when this struct is part of an array!
+    TickData(PriceType price, int volume, int timestamp)
+        : price{price}, volume{volume}, timestamp{timestamp} {}
 };
 
 template <typename PriceType>
@@ -24,6 +28,7 @@ class VWAPCalculator
 public:
     __forceinline void insertTick(const TickData<PriceType> &tickData)
     {
+#if TICK_WINDOW_QUEUE == 1
         while (!tickWindow.empty() && tickData.timestamp - tickWindow.front().timestamp >= windowSeconds)
         {
             const auto &old = tickWindow.front();
@@ -34,6 +39,23 @@ public:
         tickWindow.push(tickData);
         VWAPSum += tickData.price * tickData.volume;
         totalVolume += tickData.volume;
+#else
+        while (size && tickData.timestamp - tickWindow[head].timestamp >= windowSeconds)
+        {
+            const auto &old = tickWindow[head];
+            VWAPSum -= old.price * old.volume;
+            totalVolume -= old.volume;
+            head++;
+            head %= MAX_TICKS;
+            --size;
+        }
+        tickWindow[tail] = tickData;
+        VWAPSum += tickData.price * tickData.volume;
+        totalVolume += tickData.volume;
+        ++tail;
+        tail %= MAX_TICKS;
+        ++size;
+#endif
     }
 
     __forceinline double getVWAP() const
@@ -41,7 +63,8 @@ public:
         return VWAPSum / totalVolume;
     }
 
-    VWAPCalculator(int windowSeconds) : windowSeconds{windowSeconds}, VWAPSum{0}, totalVolume{0}
+    VWAPCalculator(int windowSeconds)
+        : windowSeconds{windowSeconds}, VWAPSum{0}, totalVolume{0}, head{0}, tail{0}, size{0}
     {
         // TODO: tickWindow queue resizing ?? it's according to timestamp though, so maybe size can be indefinite?
     }
@@ -54,12 +77,14 @@ private:
     int windowSeconds;
     double VWAPSum;
     int totalVolume;
+#if TICK_WINDOW_QUEUE == 1
     std::queue<TickData<PriceType>> tickWindow; //! Never use std::queue in HFT since it is backed by std::deque, this causes dynamic allocations and non-contiguous memory.
-
-    // std::array<TickData<PriceType>, MAX_TICKS> tickWindow; //? Use this instead. This represents a ring-buffer. It offers us ZERO dynamic memory allocations, constant time push/pop and predictable memory layout.
-    // size_t head = 0;
-    // size_t tail = 0;
-    // size_t size = 0;
+#else
+    TickData<PriceType> tickWindow[MAX_TICKS]; //? Use this instead. This represents a ring-buffer. It offers us ZERO dynamic memory allocations, constant time push/pop and predictable memory layout.
+#endif
+    size_t head;
+    size_t tail;
+    size_t size;
 };
 
 #endif
