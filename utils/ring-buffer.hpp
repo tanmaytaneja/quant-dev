4bc9ed5a34487c6eb34e63340a6da477ef7ea838
Tanmay Taneja <tanmaytaneja2@gmail.com>
2025-05-26 21:06:25 +0530

    added compiler specific FORCE_INLINE

--- utils/ring-buffer.hpp
+++ utils/ring-buffer.hpp
@@ -1,6 +1,14 @@
 #ifndef RING_BUFFER_HPP
 #define RING_BUFFER_HPP
 
+#ifdef _MSC_VER
+#define FORCE_INLINE __forceinline
+#elif defined(__GNUC__) || defined(__clang__)
+#define FORCE_INLINE inline __attribute__((always_inline))
+#else
+#define FORCE_INLINE inline
+#endif
+
 #include <iostream>
 
 constexpr size_t next_power_of_two(size_t n)
@@ -27,40 +35,47 @@ public:
         static_assert((Capacity & Mask) == 0, "Capacity of RingBuffer must be a power of 2");
     }
 
-    __forceinline bool empty() const
+    FORCE_INLINE bool empty() const
     {
         return !count;
     }
 
-    __forceinline bool full() const
+    FORCE_INLINE bool full() const
     {
         return count == Capacity;
     }
 
-    __forceinline size_t size() const
+    FORCE_INLINE size_t size() const
     {
         return count;
     }
 
-    __forceinline void push(const T &item)
+    FORCE_INLINE void push(const T &item)
     {
         buffer[tail] = item;
         tail = (tail + 1) & Mask;
         ++count;
     }
 
-    __forceinline void pop()
+    FORCE_INLINE void push(const T&& item)
+    {
+        buffer[tail] = std::move(item);
+        tail = (tail + 1) & mask;
+        ++count;
+    }
+
+    FORCE_INLINE void pop()
     {
         head = (head + 1) & Mask;
         --count;
     }
 
-    __forceinline T &front()
+    FORCE_INLINE T &front()
     {
         return buffer[head];
     }
 
-    __forceinline const T &front() const
+    FORCE_INLINE const T &front() const
     {
         return buffer[head];
     }
@@ -75,3 +90,24 @@ private:
 };
 
 #endif
+
+//! What are SSO strings?
+
+//* Can remove count, since head - tail is the same thing. Moreover, the 'count' variable can be placed in a different cache line, which is more latency sensitive compared to subtraction (head - tail).
+
+//* In tight, branchless HFT code, fewer memory read/writes are better than extra arithmetic.
+
+//* if head and tail are present in the same cache line, then multiple threads accessing them may cause cache line thrashing since both those threads will interfere in each other's cache lines. This can impact latency. Therefore, it is beneficial to alignas(64) head and tail, so that they are not present in the same cache line.
+//! but won't we be wasting cache lines then? What if only a single thread is using it? Then which one is better?
+//? Yes, technically you are wasting memory — but only a tiny amount (e.g., a few bytes per 64-byte cache line).
+//? But in HFT, you prioritize latency over memory. Cache line thrashing is far more costly than a little memory waste.
+
+//* std::atomic<size_t> head and tail will make the queue good for SPSC (one pushes and one pops).
+//! fails for SPMC, MPSC, MPMC. This will require (compare_and_swap / atomic_flags) / memory ordering control (memory_order_acquire / release)
+//! can also use algos like disruptor or libraries like folly::MPMCQueue
+
+//* __builtin_prefetch loads the memory in the cache line already before we use it!
+//? __builtin_prefetch(const void* addr, int rw = 0, int locality = 3)
+//? addr: address to prefetch, rw: 0(read)/1(write), locality: 0-3 (higher means data will be used more!)
+
+//! Understand the entire virtual function + vtable thing!
